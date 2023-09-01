9b954cc55173081961f0b07cc54ed9f48e482779
ThejasNU <thejasnu@gmail.com>
2023-09-02 01:52:21 +0530

    added health checker

--- round_robin.go
+++ round_robin.go
@@ -1,7 +1,14 @@
 package roundRobin
 
-import "sync"
+import (
+	"log"
+	"net"
+	"net/url"
+	"sync"
+	"time"
+)
 
+// ############ STRUCTS #######################################################
 type LoadBalancer struct {
 	Port    string
 	Servers []Server
@@ -10,11 +17,12 @@ type LoadBalancer struct {
 }
 
 type Server struct {
-	Port     string
+	Url    string
 	IsAlive bool
 	mu      sync.RWMutex
 }
 
+// ############ HELPERS #######################################################
 func (server *Server) GetAliveStatus() bool {
 	server.mu.RLock()
 	defer server.mu.RUnlock()
@@ -30,23 +38,77 @@ func (server *Server) SetAliveStatus(currentStatus bool) {
 	server.IsAlive = currentStatus
 }
 
-func (lb *LoadBalancer) GetNextServer() *Server{
+// ############ LOAD BALANCER MAIN FUNCTION #######################################################
+func (lb *LoadBalancer) GetNextServer() *Server {
 	lb.mu.Lock()
 	defer lb.mu.Unlock()
-	
-	numServers:= len(lb.Servers)
-	nextIndex:=(lb.index+1)%numServers
+
+	numServers := len(lb.Servers)
+	nextIndex := (lb.index + 1) % numServers
 
 	//find next server which is alive, if all servers are dead return nil
-	var count int=0
-	for lb.Servers[nextIndex].GetAliveStatus()==false && count<numServers{
-		nextIndex=(nextIndex+1)%numServers
+	var count int = 0
+	for lb.Servers[nextIndex].GetAliveStatus() == false && count < numServers {
+		nextIndex = (nextIndex + 1) % numServers
 		count++
 	}
-	
-	if count==numServers{
+
+	if count == numServers {
 		return nil
 	} else {
 		return &lb.Servers[nextIndex]
+	}
+}
+
+//############ HEALTH CHECKER #######################################################
+func (lb *LoadBalancer)checkHealth(){
+	t:=time.NewTicker(time.Minute*1)
+	
+	for{
+		select{
+		case <-t.C:
+			for i:=0;i<len(lb.Servers);i++{
+				var server *Server=&lb.Servers[i]
+				//make sure that server details are not being updated
+				server.mu.RLock()
+				pingUrl,err:=url.Parse(server.Url)
+				if err!=nil{
+					log.Fatal(err.Error())
+				}
+				server.mu.RUnlock()
+				//call helper which pings the url
+				isAlive:=checkStatus(pingUrl)
+				//setAliveStatus has lock implementation inside it so need of lock here
+				server.SetAliveStatus(isAlive)
+			}
+		}
+	}
+}
+
+func checkStatus(url *url.URL) bool {
+	conn,err:=net.DialTimeout("tcp",url.Host,time.Second*10)
+	defer conn.Close()
+	
+	if err!=nil{
+		return false
+		} else {
+			return true
+		}
+	}
+	
+//############ MAIN FUNCTION #######################################################
+func main(){
+	//create dummy servers and add
+	var lb LoadBalancer=LoadBalancer{
+		Port: "8080",
+		Servers: []Server{},
+	}
+
+	//execute health checker on different go routine
+	go lb.checkHealth()
+
+	for i:=0;i<5;i++{
+		server:=lb.GetNextServer()
+		println(server.Url)
 	}
 }
